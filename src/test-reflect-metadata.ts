5ca3e7b030affb74ef1f3efcedfae910be34532e
Кирсанов Илья Александрович (4047222) <vtb4047222@region.vtb.ru>
2022-01-22 16:33:47 +0300

    refactor: added file for test reflect-metadata

---
+++ src/test-reflect-metadata.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+
+function Injectable(key: string) {
+  return (target: Function) => {
+    Reflect.defineMetadata(key, 1, target);
+    const meta = Reflect.getMetadata(key, target);
+    console.log(meta);
+  }
+};
+
+function Inject(key: string) {
+  return (target: Function) => {
+    const meta = Reflect.getMetadata(key, target);
+    const instance = new meta();
+    return instance;
+  }
+};
+
+function Prop(target: Object, name: string) {
+  Reflect.defineMetadata('key', 1, target);
+  const meta = Reflect.getMetadata('key', target);
+  console.log(meta);
+};
+
+@Injectable('C')
+export class C {
+  @Prop prop: number;
+};
+
+// @Injectable('D')
+// export class D {
+//   constructor(@Inject('C') c: C) { // сюда подставляется экземпляр C
+//   }
+// };
